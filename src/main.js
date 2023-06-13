9790c5a575030f02b1fbe1b957e6712dac32df54
eun0leee <eun0leee@gmail.com>
2023-06-13 23:19:23 +0900

    fix : build 에러 해결_home 파일명 변경

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './reset-css.css';
 import './main.css';
-import Home from '/src/pages/Home.js';
+import Home from './pages/Home.js';
 import clock from '/src/utils/clock.js';
 import { success, error } from '/src/utils/location-and-weather.js';
 import username from '/src/utils/username.js';
