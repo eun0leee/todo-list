b3403943260853edcc6ba68546e68b4c06398112
eun0leee <eun0leee@gmail.com>
2023-06-15 09:34:15 +0900

    fix : 지역 및 날씨 리팩토링

--- src/utils/location-and-weather.js
+++ src/utils/location-and-weather.js
@@ -5,32 +5,27 @@ const getWeather = async (lat, lon) => {
   const data = await getServerWeather(lat, lon);
 
   // render
+  const createTextElement = (innerText) => {
+    const element = document.createElement('span');
+    element.className = 'text';
+    element.innerText = innerText;
+    return element;
+  };
+
   const locationEl = document.querySelector('.location');
   const weatherEl = document.querySelector('.weather');
-  const locationText = document.createElement('span');
-  locationText.className = 'text';
-  const weatherText = document.createElement('span');
-  weatherText.className = 'text';
 
-  const locationServerData = data.name;
-  const weatherServerData = data.weather[0].main;
-  locationText.innerText = locationServerData;
-  weatherText.innerText = weatherServerData;
-
-  locationEl.append(locationText);
-  weatherEl.append(weatherText);
+  locationEl.append(createTextElement(data.name));
+  weatherEl.append(createTextElement(data.weather[0].main));
 };
 
 const success = (obj) => {
-  const latitude = obj.coords.latitude;
-  const longitude = obj.coords.longitude;
+  const { latitude, longitude } = obj.coords;
 
   getWeather(latitude, longitude);
 };
 
-const error = () => {
-  alert("we can't find you. TT. Allow location access.");
-};
+const error = () => alert("we can't find you. TT. Allow location access.");
 
 navigator.geolocation.getCurrentPosition(success, error);
 
